7b26116fdabc217251b5a5829c3d4004ecd3d641
전현준Jeonhyenjun <jhj091542@gmail.com>
2021-03-24 00:51:04 +0900

    Add files via upload

---
+++ class_JSS.py
@@ -0,0 +1,42 @@
+#Thanks for watching my commit
+#Run all code after uncomment processing
+#For error inquiries, please contact jhj091542@gmail.com
+
+
+
+#전 대리가 처음 만든 class
+#class JSS():
+#    def __init__(self):
+#        self.name = input("이름")
+#        self.age = input("나이")
+
+#쉽게 설명하는 class
+#class JSS():
+#    def __init__(self):
+#        print("안녕")
+#    def show(self):
+#       print("show 실행!")
+#a = JSS()
+#a.show()    
+   
+#현실적인 코드
+#class JSS():
+#    def __init__(self):
+#        self.name = input("이름")
+#        self.age = input("나이")
+#    def show(self):
+#        print("나의 이름은 {} , 나이는 {}살입니다.".format(self.name,self.age))
+#a = JSS()
+#a.show()        
+
+#상속
+#class JSS2(JSS):
+#    def __init__(self):
+#        super().__init__()
+#       self.gender = input("성별")
+#a = JSS2()
+
+#pass 
+#class JSS2(JSS):
+#    def __init__(self):
+#        pass
